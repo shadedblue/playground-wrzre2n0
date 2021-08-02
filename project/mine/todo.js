e56fdf164a6ac5f5693d3b48a2b9c18d265c4eba
Nate Hapke <nathan.hapke@gmail.com>
2021-08-02 12:53:49 -0700

    my first page

--- project/mine/todo.js
+++ project/mine/todo.js
@@ -2,16 +2,16 @@ function getWeights() {
 	//TODO !
 	// Recommended values: 4/8, 3/8, 1/8
 	return {
-		separation: 0/8, 
-		alignment: 0/8,
-		cohesion: 0/8,
+		separation: 4/8, 
+		alignment: 3/8,
+		cohesion: 1/8,
 	};
 }
 
 function getBoidViewDistance() {
 	//TODO !
 	// Recommended value: 50
-	return 0;
+	return 50;
 }
 
 function getMaxForce() {

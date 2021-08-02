07a7060bdc83d38aabb94d1a9e7ea2dfbf8c0278
Nate Hapke <nathan.hapke@gmail.com>
2021-08-02 12:36:11 -0700

    initial load from source

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# techio-basic-template
+# A Tech.io playground about Flocking, a type of Swarm Intelligence for AI
+
+See runnable content on [Tech.io](https://tech.io/playgrounds/1003/flocking-autonomous-agents)
---
+++ project/demo/script.js
@@ -0,0 +1,92 @@
+Boid.prototype.flock = function (boids, delta) {
+	// Find nearby boids
+	var hood = this.getNeighbourhood(boids);
+	// Calculate flocking forces
+	var sep = this.separation(hood);
+	var ali = this.alignment(hood);
+	var coh = this.cohesion(hood);
+
+
+	// Apply weights to forces	
+	sep.multiplyScalar(weights.separation);
+	ali.multiplyScalar(weights.alignment);
+	coh.multiplyScalar(weights.cohesion);
+
+	// Apply forces to boid
+	this.acceleration.add(sep).add(ali).add(coh);
+	if (!this.acceleration.isZero()) {
+		this.acceleration.normalize().multiplyScalar(weights.maxForce);
+	}
+};
+
+Boid.prototype.separation = function (hood) {
+	var desiredSeparation = this.radius * 2 + 5;
+
+	var average = new Victor(0, 0);
+	var count = 0;
+	// For every boid in the system, check if it's too close
+	for (var i = 0, l = hood.length; i < l; ++i) {
+		var neighbour = hood[i];
+		var other = neighbour.boid;
+		var d = neighbour.distance;
+		if (d < desiredSeparation && d > 0) {
+			// Calculate vector pointing away from neighbour, weighted by distance 
+			var diff = this.position.clone().subtract(other.position).normalize().divideScalar(d);
+			average.add(diff);
+
+			count++;
+		}
+	}
+	if (count > 0) {
+		return this.steer(average.normalize().multiplyScalar(weights.desiredSpeed));
+	}
+	return average;
+};
+
+
+// Alignment
+Boid.prototype.alignment = function (hood) {
+	var average = new Victor(0, 0);
+	var count = 0;
+	for (var i = 0, l = hood.length; i < l; ++i) {
+		var neighbour = hood[i];
+		var other = neighbour.boid;
+		average.add(other.velocity);
+		count++;
+	}
+	if (count > 0 && !average.isZero()) {
+		return this.steer(average.normalize().multiplyScalar(weights.desiredSpeed));
+	}
+	return average;
+};
+
+// Cohesion
+Boid.prototype.cohesion = function (hood) {
+	var average = new Victor(0, 0);
+	var count = 0;
+
+	for (var i = 0, l = hood.length; i < l; ++i) {
+		var neighbour = hood[i];
+		var other = neighbour.boid;
+		average.add(other.position);
+		count++;
+	}
+	if (count > 0) {
+		var destination = average.divideScalar(count).subtract(this.position);
+		var dist = destination.length();
+		if (dist > 0) {
+			destination.normalize();
+		}
+		destination.multiplyScalar(weights.desiredSpeed);
+		return this.steer(destination);
+	}
+	return average;
+};
+
+Boid.prototype.decision = function (environment) {
+	this.flock(environment.boids);
+};
+
+for (var i = 0; i < 100; ++i) {
+	boids.push(new Boid(Math.random() * app.screen.width, Math.random() * app.screen.height));
+}
